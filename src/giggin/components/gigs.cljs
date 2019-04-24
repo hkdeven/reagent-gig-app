1a062c2b2e1eb4f2458ff9d7472b9075035ffbcb
Deven Blackburn <deven@makecodenotwar.co>
2019-04-24 16:40:52 -0400

    Shortcut + refactor :zap:

--- src/giggin/components/gigs.cljs
+++ src/giggin/components/gigs.cljs
@@ -4,19 +4,20 @@
 
 (defn gigs
   []
-  [:main
-   [:div.gigs
-    (for [{:keys [id img title price desc]} (vals @state/gigs)]
-      [:div.gig {:key id}
-       [:img.gig__artwork {:src img :alt title}]
-       [:div.gig__body
-        [:div.gig__title
-         [:div.btn.btn--primary.float--right.tooltip
-          {:data-tooltip "Add to order"
-           :on-click (fn [] (swap! state/orders update id inc))}
-          [:i.icon.icon--plus]] title]
-        [:p.gig__price price]
-        [:p.gig__desc desc]]])]])
+  (let [add-to-order #(swap! state/orders update % inc)]
+    [:main
+     [:div.gigs
+      (for [{:keys [id img title price desc]} (vals @state/gigs)]
+        [:div.gig {:key id}
+         [:img.gig__artwork {:src img :alt title}]
+         [:div.gig__body
+          [:div.gig__title
+           [:div.btn.btn--primary.float--right.tooltip
+            {:data-tooltip "Add to order"
+             :on-click #(add-to-order id)}
+            [:i.icon.icon--plus]] title]
+          [:p.gig__price (format-price price)]
+          [:p.gig__desc desc]]])]]))
 
    ; same method using map
    ; [:div.gigs (map (fn [gig]
